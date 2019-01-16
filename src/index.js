0b193be071ea4e90ba8512a72b97f6b5be012ac1
hurrytospring <ddyz1993@163.com>
2019-01-16 17:34:16 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,90 @@
+const methods = [];
+export default class vitebridge {
+    constructor(readyCallback, methods = methods) {
+        this._event = {};
+        this._ready = false;
+        this.registerHandlerCacheQueu = [];
+        this.callHandleCacheQueu = [];
+        //-------------- ios init------
+        const _readyCallback = (_bridge) => {
+            this._ready = true;
+            this.callHandleCacheQueu.forEach(args=>{
+                this._originBridge.callHandler(...args)
+            })
+            this.registerHandlerCacheQueu.forEach(args=>{
+                this._originBridge.registerHandler(...args)
+            })
+            typeof readyCallback === "function" && readyCallback(_bridge)
+        }
+        window.WKWVJBCallbacks = window.WKWVJBCallbacks || [_readyCallback];
+        window.webkit.messageHandlers.iOS_Native_InjectJavascript.postMessage(null);
+        // -----------------
+        methods.forEach(m => {
+            this[m] = (arg) => {
+                    return new Promise((res, rej) => {
+                        const callback=(function (res, rej) {
+                            return function (responseArgs) {
+                                const { code = 0, msg = "", data = null }=JSON.parse(responseArgs);
+                                //timeout todo
+                                if (code === 0) {
+                                    res(data)
+                                } else {
+                                    rej({ code, msg, data })
+                                }
+                            }
+                        })(res, rej)
+                        console.log(99999,callback)
+                        const callHandlerArgs = [m, arg===undefined?"":JSON.stringify(arg), callback]
+                        if (!this._ready) {
+                            console.log(`call ${m} when not ready,${JSON.stringify(callHandlerArgs)}`)
+                            this.callHandleCacheQueu.push(callHandlerArgs)
+                        } else {
+                            console.log(`call ${m} when ready,${JSON.stringify(callHandlerArgs)}`)
+                            this._originBridge.callHandler(...callHandlerArgs)
+                        }
+
+                    })
+
+
+            }
+        })
+    }
+    get _originBridge() {
+        return window.WKWebViewJavascriptBridge
+    }
+    subscribe(eventName, cb) {
+        if (typeof cb !== "function") {
+            throw new Error("callback must be a function")
+        }
+        const registered = !!this._event[eventName]
+        !registered && (this._event[eventName] = []);
+        if (this._event[eventName].some(f => {
+            return f === cb;
+        })) return;
+        this._event[eventName].push(cb);
+        if (!registered) {
+            const registerHandlerArgs = [eventName, (args) => {
+                this._event[eventName].forEach(cb => {
+                    console.log('emitter',eventName,args)
+                    cb(JSON.parse(args))
+                })
+            }]
+            if (this._ready) {
+                this._originBridge.registerHandler(...registerHandlerArgs)
+            } else {
+                this.registerHandlerCacheQueu.push(registerHandlerArgs)
+            }
+
+        }
+
+    }
+    unSubscribe(eventName, cb) {
+        if (typeof cb !== "function") {
+            throw new Error("callback must be a function")
+        }
+        const registered = !!this._event[eventName]
+        if (!registered) return;
+        this._event[eventName].splice(this._event[eventName].indexOf(cb), 1);
+
+    }
+}
---
+++ src/test.js
@@ -0,0 +1,64 @@
+import viteBridge from ".";
+const methods = ['appToken', 'appChannel', 'goLoginVC', 'goToshare', 'sendTranscation', 'fetchViteAddress'];
+const sub = ['appStatus']
+const bridge = new viteBridge(() => {
+    console.log('success-------ready 回调');
+}, methods);
+
+
+function insertTpl(testKey) {
+    const template = `<div class="tpl card ${testKey}">
+        <div class="title badge badge-secondary" style="height:30px;margin:10px auto;">test for ${ testKey} </div>
+        <div class="content" style="height:30px;margin:10px auto;border:1px solid #000;width:90%;"></div>
+        <button class="btn btn-primary" type="button" style="height:30px;margin:10px auto;">test</button>
+    </div>`
+    $("#app").append(template)
+}
+function attachClickEvent(testKey, clickEvent) {
+    const ele = document.getElementsByClassName(testKey)[0];
+    const contentEl = ele.getElementsByClassName("content")[0];
+    const button = ele.getElementsByClassName('btn')[0]
+    button.addEventListener("click", () => {
+        console.log(`trigger ${testKey}'s click event`);
+        clickEvent(contentEl);
+    })
+}
+
+function attachSub(testKey, content) {
+    const ele = document.getElementsByClassName(testKey)[0];
+    const contentEl = ele.getElementsByClassName("content")[0];
+    contentEl.textContent = content;
+}
+methods.forEach(m => {
+    insertTpl(m);
+    let arg = undefined;
+    if (m === 'goToshare') {
+        arg = { url: "https://baidu.com" }
+    }
+    if (m === 'sendTranscation') {
+        arg = {
+            "token": '{"tokenId":"tti_5649544520544f4b454e6e40","tokenName":"Vite Token","tokenSymbol":"VITE","decimals":18}',
+            "toAddress": 'vite_53c1c34fe05835ac0becb0c4c6e0aaf67c356a935bc9a7d286',
+            "amount": "100",
+            "note": "test"
+        }
+    }
+    if (m === 'fetchViteAddress') {
+        arg = { data: "vite" }
+    }
+    attachClickEvent(m, (contentEl) => {
+        bridge[m](arg).then(res => {
+            console.log(`${m} response success!!!!${JSON.stringify(res)}`)
+            contentEl.textContent = JSON.stringify(res);
+        }, rej => {
+            console.log(`${m} response fail!!!!${rej}`)
+        })
+    })
+})
+sub.forEach(s => {
+    insertTpl(s);
+    bridge.subscribe(s, function (reponse) {
+        console.log('ffff')
+        attachSub(m, `${m} event has triggerd,reponse is \n ${JSON.stringify(reponse)}`)
+    })
+})
